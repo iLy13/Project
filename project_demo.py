3392ab8f64832b77bbcb8df51f0edbebecd26dad
iLy13 <egor.korneeko@gmail.com>
2022-01-05 22:35:09 +0700

    Музыка для теста

--- project_demo.py
+++ project_demo.py
@@ -8,6 +8,7 @@ from pygame.display import set_gamma_ramp
 from pygame.math import Vector2
 
 pygame.init()
+pygame.mixer.music.load('data/morgen.mp3')
 size = width, height = 1000, 1000
 fps = 60
 clock = pygame.time.Clock()
@@ -27,7 +28,7 @@ def terminate():
 def load_image(name, colorkey=None):
     fullname = os.path.join('data', name)
     if not os.path.isfile(fullname):
-        print(f"Файл с изображением '{fullname}' не найдеj")
+        print(f"Файл с изображением '{fullname}' не найден")
         sys.exit()
     image = pygame.image.load(fullname)
     if colorkey is not None:
@@ -41,27 +42,17 @@ def load_image(name, colorkey=None):
 
 
 def start_screen():
-    intro_text = []
-
     fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
     screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, True, pygame.Color('black'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 terminate()
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                return
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+                return 1
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_2:
+                return 2
+
         pygame.display.flip()
         clock.tick(fps)
 
@@ -263,14 +254,18 @@ def generate_level(level):
 
 def main():
     running = True
-    start_screen()
-    player, level_x, level_y, enemies = generate_level(load_level('level.txt'))
+    level = start_screen()
+    if level == 1:
+        player, level_x, level_y, enemies = generate_level(load_level('level.txt'))
+    elif level == 2:
+        player, level_x, level_y, enemies = generate_level(load_level('level2.txt'))
+    pygame.mixer.music.play()
     for i in range(len(enemies)):
         enemy = enemies[i]
     camera = Camera()
     while running:
         pygame.mouse.set_visible(False)
-        screen.fill((255, 255, 255))
+        screen.fill((0, 0, 0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False

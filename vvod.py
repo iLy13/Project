19456cfedbc3ef669f80d9d322eb431a60331d6e
Skublik <82895701+Skublik-glek@users.noreply.github.com>
2022-01-16 17:07:28 +0300

    модификация

--- vvod.py
+++ vvod.py
@@ -3,17 +3,35 @@ import re
 import pygame as pg
 
 class Pole_vvoda():
-    def __init__(self, x, y, width, height) -> None:
+    def __init__(self, x, y, width, height, limit) -> None:
         self.font = pg.font.Font(None, 20)
         self.input_box = pg.Rect(x, y, width, height)
         self.color = pg.Color('white')
         self.text = ''
         self.active = False
+        self.limit = limit
 
-    def draw(self, text, screen):
+    def draw(self, screen):
+        
         txt_surface = self.font.render(self.text, True, self.color)
         screen.blit(txt_surface, (self.input_box.x + 5, self.input_box.y + 5))
         pg.draw.rect(screen, self.color, self.input_box, 2)
+
+    def update(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if self.get_box().collidepoint(event.pos):
+                self.change_act(not self.get_act())
+            else:
+                self.change_act(False)
+        if event.type == pg.KEYDOWN:
+            if self.get_act():
+                if event.key == pg.K_RETURN:
+                    print(self.get_text())
+                    self.change_text('')
+                elif event.key == pg.K_BACKSPACE:
+                    self.change_text(self.get_text()[:-1])
+                else:
+                    self.change_text(self.get_text() + event.unicode)
 
     def get_act(self):
         return self.active
@@ -25,7 +43,8 @@ class Pole_vvoda():
         self.active = act
 
     def change_text(self, text):
-        self.text = text
+        if len(text) <= self.limit:
+            self.text = text
     
     def get_text(self):
         return self.text
@@ -34,29 +53,16 @@ class Pole_vvoda():
 pg.init()
 screen = pg.display.set_mode((640, 480))
 clock = pg.time.Clock()
-text_input = Pole_vvoda(100, 100, 300, 30)
+text_input = Pole_vvoda(100, 100, 300, 30, 10)
 done = False
 
 while done == False:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
-        if event.type == pg.MOUSEBUTTONDOWN:
-            if text_input.get_box().collidepoint(event.pos):
-                text_input.change_act(not text_input.get_act())
-            else:
-                text_input.change_act(False)
-        if event.type == pg.KEYDOWN:
-            if text_input.get_act():
-                if event.key == pg.K_RETURN:
-                    print(text_input.get_text())
-                    text_input.change_text('')
-                elif event.key == pg.K_BACKSPACE:
-                    text_input.change_text(text_input.get_text()[:-1])
-                else:
-                    text_input.change_text(text_input.get_text() + event.unicode)
+        text_input.update(event)
     screen.fill((30, 30, 30))
-    text_input.draw(text, screen)
+    text_input.draw(screen)
     pg.display.flip()
     clock.tick(30)
 
